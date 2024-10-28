c459f8c354d2f4a04871fe4c3fa86ca01420c71c
yamayuba00 <yamayuba00@gmail.com>
2024-10-28 14:11:26 +0700

    Add Favicon PRSI

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title> @yield('title') - PRSI </title>
+    <link rel="shortcut icon" href="{{ asset('img/prsi.png') }}" type="image/x-icon">
 
     @stack('seo')
 
