330b78789caa42aec26c430ad563e80a23df3d4e
yamayuba00 <yamayuba00@gmail.com>
2024-10-28 23:01:53 +0700

    Fix Image

--- resources/views/components/sponsor/view.blade.php
+++ resources/views/components/sponsor/view.blade.php
@@ -15,13 +15,13 @@
                 {{ $sponsorship === 'sponsors' ? 'Sponsor Utama' : 'Co-Sponsor' }}
             </h4>
         </div>
-        <div class="bg-white shadow rounded-sm p-4 mt-4">
+        <div class="bg-white shadow rounded-sm p-0 mt-4">
             {{-- <div class="grid lg:grid-cols-6 md:grid-cols-4 grid-cols-2 gap-3">
                 @foreach ($sponsorshipData as $item)
                     <x-custom.sponsorship-image :sponsorship="$item" :textAlt="$sponsorship === 'sponsors' ? 'Sponsor' : 'Co-Sponsor'" />
                 @endforeach
             </div> --}}
-            <img src="{{ asset('img/sponsor.jpg') }}" alt="Sponsor" class="w-full h-64 object-contain">
+            <img src="{{ asset('img/sponsor.jpg') }}" alt="Sponsor" class="w-full lg:h-64 h-72 object-contain">
         </div>
     </div>
 </div>
