505f60fe7f435afd7c316d709823378c3105955e
yamayuba00 <yamayuba00@gmail.com>
2024-10-28 13:19:07 +0700

    Fix Image Sponsor / CoSponsor

--- resources/views/components/custom/sponsorship-image.blade.php
+++ resources/views/components/custom/sponsorship-image.blade.php
@@ -1,5 +1,5 @@
 @props(['sponsorship', 'textAlt'])
 <div>
-    <img src="{{ asset('storage/' . $sponsorship->image) }}" class="md:w-full md:h-16 h-16 w-full object-fill"
+    <img src="{{ asset('storage/' . $sponsorship->image) }}" class="md:w-full md:h-16 h-16 w-full object-fit"
         alt="{{ $textAlt }}-{{ $sponsorship->image }}" />
 </div>
