6b52952ad9d52a63f7469bcf118b63ab068dfb6c
yamayuba00 <yamayuba00@gmail.com>
2024-10-28 21:27:02 +0700

    Fix Image v3

--- resources/views/components/custom/sponsorship-image.blade.php
+++ resources/views/components/custom/sponsorship-image.blade.php
@@ -1,6 +1,6 @@
 @props(['sponsorship', 'textAlt'])
 <div class="flex justify-center items-center">
     <img src="{{ asset('storage/' . $sponsorship->image) }}" 
-         class="lg:h-16 h-16 w-full object-contain" 
+         class="lg:h-12 h-12 w-full object-contain" 
          alt="{{ $textAlt }}-{{ $sponsorship->image }}" />
 </div>
